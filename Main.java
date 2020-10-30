596335180fc388445181273bff0a9fe7c07eb5d0
ellag7121 <ellag7121@wrdsb.ca>
2020-10-30 18:02:57 +0000

    finished a8q4!

--- Main.java
+++ Main.java
@@ -5,13 +5,20 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
 
+  JLabel numberLabel;
+  JLabel factoralLabel;
+
+  JTextField numberField;
+  JTextField factoralDisplay;
+
+  JButton calcButton;
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Factoral Calculator");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
@@ -19,7 +26,41 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+
+    //initialize the class Variables
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null);
+
+    numberLabel = new JLabel("n");
+    factoralLabel = new JLabel("factoral(n)");
+
+    numberField = new JTextField();
+    factoralDisplay = new JTextField();
+
+    calcButton = new JButton("CALC");
+
+    //set location and size of class Variables
+    numberLabel.setBounds(20,20,10,20);
+    numberField.setBounds(40,20,50,20);
+    factoralLabel.setBounds(100,20,80,20);
+    factoralDisplay.setBounds(190,20,50,20);
+    calcButton.setBounds(250,20,80,20);
+
+    //add an action ActionListener to the button
+    calcButton.addActionListener(this);
+
+    //Disable the factoralDisplay
+    factoralDisplay.setEnabled(false);
+
+    //add the components to the main panel
+    mainPanel.add(numberLabel);
+    mainPanel.add(numberField);
+    mainPanel.add(factoralLabel);
+    mainPanel.add(factoralDisplay);
+    mainPanel.add(calcButton);
+
+    //add main panel to frame
+    frame.add(mainPanel);
 
   }
 
@@ -27,6 +68,18 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    //make a variable to hold the entered number (this will also be an accumulator for the final factoral)
+    int number = Integer.parseInt(numberField.getText());
+
+
+    //make a for loop to multiply the number by all it's consecutive decreasing integers
+    for(int i = number; i > 1; i--){
+      //change the number to itself time the current value of "i" minus one
+      number *= i - 1;
+    }
+    //print the factoral to the factoralDisplay
+    factoralDisplay.setText("" + number);
 
   }
 
